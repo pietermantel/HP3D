4d0e9e7353667eeedebe18a12a1c9d6f0cf820bc
pietermantel <4pietermantel@gmail.com>
2020-08-27 10:24:18 +0200

    Model Loading #2

--- src/engine/models/Polygon.java
+++ src/engine/models/Polygon.java
@@ -1,12 +1,9 @@
 package engine.models;
 
-import java.awt.Image;
-
 import engine.math.Point2D;
 import engine.math.Point3D;
 
 public class Polygon {
 	private Point3D[] vertices;
 	private Point2D[] textureCoords;
-	private Image texture;
 }
